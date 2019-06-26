cc9e5a36e70a3fa8848dac5456149e57e3a1c489
admin <mailtosungkwon@gmail.com>
2019-06-26 13:46:25 +0100

    Set up models and basic view / routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RoomListComponent } from './rooms/room-list/room-list.component';
+import { RoomItemComponent } from './rooms/room-list/room-item/room-item.component';
+import { RoomInsideComponent } from './rooms/room-list/room-inside/room-inside.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RoomsComponent },
+  { path: 'room/:id', component: RoomInsideComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent,
+    RoomListComponent,
+    RoomItemComponent,
+    RoomInsideComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
