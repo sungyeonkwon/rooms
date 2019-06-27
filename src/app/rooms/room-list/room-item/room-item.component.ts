4c1ff90f9e2e2332c91f496c9c0f9e6ee9806f4a
admin <mailtosungkwon@gmail.com>
2019-06-27 14:37:38 +0100

    Make animation func basics

--- src/app/rooms/room-list/room-item/room-item.component.ts
+++ src/app/rooms/room-list/room-item/room-item.component.ts
@@ -1,4 +1,4 @@
-import { Input, Component, OnInit } from '@angular/core';
+import { Input, Component, ElementRef, AfterViewInit, ViewChild, OnInit, Renderer2, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Room } from '../../room.model';
 
@@ -7,16 +7,26 @@ import { Room } from '../../room.model';
   templateUrl: './room-item.component.html',
   styleUrls: ['./room-item.component.scss']
 })
-export class RoomItemComponent implements OnInit {
+export class RoomItemComponent implements AfterViewInit {
+
   @Input() room: Room;
 
-  private router: Router;
+  constructor(
+    private router: Router,
+    private el: ElementRef,
+    private renderer: Renderer2
+    ) { }
 
-  constructor(router: Router) { 
-    this.router = router
-  }
+  ngAfterViewInit() {
+    // this.renderer.setStyle(this.el.nativeElement, 'width', '25%');
+    console.log("tha", this.el.nativeElement.querySelector('a'))
+    const roomWrapper = this.el.nativeElement.querySelector('app-room-item ')
 
-  ngOnInit() {
+    setTimeout(() => {
+      // console.log('gota', roomWrapper)
+      this.renderer.addClass(this.el.nativeElement, 'active');
+    },2000)
+
   }
 
   onRoomClick() {
