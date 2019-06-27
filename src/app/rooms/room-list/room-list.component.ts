1e723dcebcf228786aceb4c4a4c88b5cd7743b76
admin <mailtosungkwon@gmail.com>
2019-06-27 13:49:18 +0100

    Revert "Error on animation"
    This reverts commit 1b4f6dbf6edd1c98459df1d512078da7a04f3347.

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -1,7 +1,4 @@
-import { Component, 
-  OnInit,
-} from '@angular/core';
-import { trigger, transition, state, group, keyframes, animate, style } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 import { Room } from '../room.model'
 import { RoomService } from '../room.service'
@@ -10,40 +7,15 @@ import { RoomService } from '../room.service'
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
   styleUrls: ['./room-list.component.scss'],
-  animations: [
-  ]
 })
 export class RoomListComponent implements OnInit {
-
   rooms: Room[];
-  roomPackages = []
 
-  constructor(
-    private roomService: RoomService
-  ) { }
+  constructor(private roomService: RoomService) { }
 
   ngOnInit() {
     this.rooms = this.roomService.getRooms()
-    
-    // Package rooms into 4 for animation
-    let roomPackageItem: Room[] = []
-    for (let i = 1; i <= this.rooms.length; i++){
-      roomPackageItem.push(this.rooms[i-1])
-      if (i % 4 === 0 || i == this.rooms.length){
-        this.roomPackages.push(roomPackageItem)
-        roomPackageItem = []
-      }
-    }
-    
+    console.log("[room-list] this.rooms", this.rooms)
   }
-
-  onClickTest(){
-    console.log("clieck next")
-  }
-
-
 
 }
-
-
-
--- src/app/rooms/room.service.ts
+++ src/app/rooms/room.service.ts
@@ -7,16 +7,10 @@ export class RoomService {
   private rooms: Room[] = [
     new Room(1, 'Room 1', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
     new Room(2, 'Room 2', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(3, 'Roo3rm 3', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(4, 'Ro3r3om 4', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(5, 'Ro3r3rom 5', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(6, 'Ro3rom 6', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(7, 'Room 7', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(8, 'Room 8', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(9, 'Ro 9', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(10, 'Ro3r10', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(11, 'Ro3r3rom 11', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
-    new Room(12, 'Ro3rom 12', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
+    new Room(3, 'Roo3rm 1', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
+    new Room(4, 'Ro3r3om 2', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
+    new Room(5, 'Ro3r3rom 1', 'Jungle theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
+    new Room(6, 'Ro3rom 2', 'seoncd theme.', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg'),
   ]
 
   getRooms() {
